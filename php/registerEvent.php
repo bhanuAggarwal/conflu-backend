4c356827cbcd32db0f9db06590ff9703b73ec00e
bhanuAggarwal <bhanuaggarwal13@gmail.com>
2015-01-13 23:04:44 +0530

    Minnor Changes

--- php/registerEvent.php
+++ php/registerEvent.php
@@ -2,12 +2,24 @@
 	require_once("database.php");
 	session_start();
 	if(isset($_SESSION['username'])) {
+		$username = addslashes($_SESSION['username']);
+		$eventName = addslashes($_POST['eventName']);
+		$eventdescription = addslashes($_POST['eventdescription']);
+		$eventRules = addslashes($_POST['eventRules']);
+		$coordinator1 = addslashes($_POST['coordinator1']);
+		$coordinator2 = addslashes($_POST['coordinator2']);
+		$phone1 = addslashes($_POST['phone1']);
+		$phone2 = addslashes($_POST['phone2']);
+		$time = addslashes($_POST['time']);
+		$venue = addslashes($_POST['venue']);
+		$category = addslashes($_POST['category']);
 		if(empty($_POST['eventid'])) {
 			//Register new Event
+			$_POST['eventName'] = addslashes($_POST['eventName']);
 			$sql = "insert into events(author,eventname,description,rules,coordinator1,coordinator2,phone1,phone2,
-					time,venue,category) values('$_SESSION[username]','$_POST[eventName]','$_POST[eventdescription]'
-					,'$_POST[eventRules]','$_POST[coordinator1]','$_POST[coordinator2]','$_POST[phone1]'
-					,'$_POST[phone2]','$_POST[time]','$_POST[venue]','$_POST[category]')";
+					time,venue,category) values('$username','$eventName','$eventdescription'
+					,'$eventRules','$coordinator1','$coordinator2','$phone1'
+					,'$phone2','$time','$venue','$category')";
 			
 			$result = $db->query($sql);
 			//echo $result;
@@ -16,10 +28,10 @@
 		}
 		else {
 			//Update the Existing Event
-			$sql = "update events set eventname = '$_POST[eventName]',description = '$_POST[eventdescription]', rules = '$_POST[eventRules]'
-					,coordinator1 = '$_POST[coordinator1]', coordinator2 = '$_POST[coordinator2]'
-					,phone1 = '$_POST[phone1]',phone2 = '$_POST[phone2]'
-					,time = '$_POST[time]',venue = '$_POST[venue]',category = '$_POST[category]' 
+			$sql = "update events set eventname = '$eventName',description = '$eventdescription', rules = '$eventRules'
+					,coordinator1 = '$coordinator1', coordinator2 = '$coordinator2'
+					,phone1 = '$phone1',phone2 = '$phone2'
+					,time = '$time',venue = '$venue',category = '$category' 
 					where eventid = '$_POST[eventid]'";
 			
 			$result = $db->query($sql);
